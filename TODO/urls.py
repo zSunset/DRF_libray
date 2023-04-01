7edb90473fb9328fd698a96c8a4115e3bafdc988
zSunset <85899844+zSunset@users.noreply.github.com>
2023-04-01 21:18:32 +0300

    lesson_9 (#8)

--- TODO/urls.py
+++ TODO/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from .views import ProjectModelViewSet, ToDo_notesModelViewSet, CustomUserModelViewSet
 from .apps import TodoConfig
 from rest_framework.routers import DefaultRouter
 
 app_name = TodoConfig.name
 
+router = DefaultRouter()
+router.register('project', ProjectModelViewSet)
+router.register('todonotes', ToDo_notesModelViewSet)
+router.register('authors', CustomUserModelViewSet)
 
 urlpatterns = [
-    path('views/api-view', views.ProjectModelViewSet.as_view(), namespace='vapi')
+    path('', include(router.urls)),
 ]
--- config/urls.py
+++ config/urls.py
@@ -6,21 +6,36 @@ from TODO import views as views_todo
 from TODO.views import CustomUserModelViewSet, ProjectModelViewSet, ToDo_notesModelViewSet
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 from rest_framework.authtoken import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-router = DefaultRouter()
-router.register('authors', AuthorModelViewSet)
-router.register('appuser', CustomUserModelViewSet)
-router.register('project', ProjectModelViewSet)
-router.register('todonotes', ToDo_notesModelViewSet)
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="TODO API",
+      default_version='v1',
+      description="Test description",
+      contact=openapi.Contact(email="logist26rus@gmail.com"),
+      license=openapi.License('MIT'),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth', include('rest_framework.urls')),
     path('views/api-view', views_todo.ProjectModelViewSet.as_view({'get': 'list'})),
-    path('', include(router.urls)),
+    path('api/', include('TODO.urls')),
     path('api-token-auth/', views.obtain_auth_token),
-    #path('api/auth/', include('djoser.urls')),
-    #re_path(r'^auth/', include('djoser.urls.authtoken')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+
 
 ]
