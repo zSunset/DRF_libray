1939cf14c8a6c77c94920ecdaf4d76ea33ac8ee4
zSunset <logist26rus@icloud.com>
2023-04-05 14:10:59 +0300

    lesson_11

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import axios from 'axios';
 import CustomUserList from './components/customuser';
@@ -7,6 +7,8 @@ import ProjectList from './components/project';
 import TodoNotesList from './components/todonotes';
 import {Route, BrowserRouter, Link, Routes} from 'react-router-dom';
 import Auth from './components/Auth';
+import ProjectForm from './components/ProjectForm';
+import TodoForm from './components/TodoNotesForms';
 
 class  App extends React.Component {
     constructor(props) {
@@ -62,10 +64,54 @@ class  App extends React.Component {
         return console.log('Hueva')
     }
 
+    createProject(name, link_to_repository, user_set) {
+        let headers = this.getHeaders()
+        
+        axios
+            .post('http://127.0.0.1:8000/api/project/', {'name': name, 'link_to_repository': link_to_repository, 'user_set': user_set},{headers})
+            .then(response => {
+                this.getData()
+            })
+            .catch(error => console.log(error))
+        }
+
+    createTodoNotes(header, body, user_set) {
+        let headers = this.getHeaders()
+        
+        axios.post('http://127.0.0.1:8000/api/todonotes/', {'header': header, 'body': body, 'user_set': user_set},{headers})
+            .then(response => {
+                this.getData()
+            })
+            .catch(error => console.log(error))
+        }
+    
+
+
+    deleteProject(projectId) {
+        let headers = this.getHeaders()
+        
+        axios
+            .delete(`http://127.0.0.1:8000/api/project/${projectId}`, {headers})
+            .then(response => {
+                this.setState({project: this.state.project.filter((item) => item.id !== projectId)})
+            }).catch(error => console.log(error))
+        }
+        
+    deleteToDoNotes(todoNotesId) {
+        let headers = this.getHeaders()
+        axios
+            .delete(`http://127.0.0.1:8000/api/todonotes/${todoNotesId}`, {headers})
+            .then(response => {
+                this.setState({todonotes: this.state.todonotes.filter((item) => item.id !== todoNotesId)})
+            }).catch(error => console.log(error))
+    }
+    
+    
+    
     getData() {
         let headers = this.getHeaders()
         axios
-            .get('http://127.0.0.1:8000/appuser/', {headers})
+            .get('http://127.0.0.1:8000/api/authors/', {headers})
             .then(response => {
                 const custom_user = response.data
                     this.setState(
@@ -76,7 +122,7 @@ class  App extends React.Component {
             })
             .catch(error => console.log(error))
         axios
-            .get('http://127.0.0.1:8000/project/', {headers})
+            .get('http://127.0.0.1:8000/api/project/', {headers})
             .then(response => {
                 const project = response.data
                     this.setState(
@@ -87,7 +133,7 @@ class  App extends React.Component {
             })
             .catch(error => console.log(error))
         axios
-            .get('http://127.0.0.1:8000/todonotes/', {headers})
+            .get('http://127.0.0.1:8000/api/todonotes/', {headers})
             .then(response => {
                 const todonotes = response.data
                     this.setState(
@@ -115,12 +161,16 @@ class  App extends React.Component {
                     <li> <Link to='/custom_user'>CustomUser</Link></li>
                     <li> <Link to='/project'>Project</Link></li>
                     <li> <Link to='/todonotes'>TodoNotes</Link></li>
+                    <li> <Link to='/create_project'>Create Project</Link></li>
+                    <li> <Link to='/create_todo_notes'>Create TodoNotes</Link></li>
                     <li> {this.isAuth() ? <button onClick={() => this.logOut()}>Logout</button> : <Link to='/login'>Login</Link> }</li>
                 </nav>
                     <Routes>
                         <Route exact path='/custom_user' element={<CustomUserList custom_user={this.state.custom_user}/>} />
-                        <Route exact path='/project' element={<ProjectList project={this.state.project}/>} />
-                        <Route exact path='/todonotes' element={<TodoNotesList todonotes={this.state.todonotes}/>} />
+                        <Route exact path='/project' element={<ProjectList project={this.state.project}  deleteProject={(projectId) => this.deleteProject(projectId)}/>}/>
+                        <Route exact path='/todonotes' element={<TodoNotesList todonotes={this.state.todonotes} deleteToDoNotes={(todoNotesId) => this.deleteToDoNotes(todoNotesId)}/>}/>
+                        <Route exact path='/create_todo_notes' element={<TodoForm custom_user={this.state.custom_user} createTodoNotes={(header, body, custom_user) => this.createTodoNotes(header, body, custom_user)}/>}/>
+                        <Route exact path='/create_project' element={<ProjectForm custom_user={this.state.custom_user} createProject={(name, link_to_repository, custom_user) => this.createProject(name, link_to_repository, custom_user)}/>}/>
                         <Route exact path='/login' element={<Auth obtainAuthToken={(login, password) => this.obtainAuthToken(login, password)}/>} />
                     </Routes>
                 </BrowserRouter>    
--- frontend/src/components/todonotes.js
+++ frontend/src/components/todonotes.js
@@ -1,22 +1,25 @@
 import React from 'react';
 
-const TodoNotesItem = ({todonotes}) => {
+const TodoNotesItem = ({todonotes, deleteToDoNotes}) => {
     return (
         <tr>
+            <td>{todonotes.id}</td>
             <td>{todonotes.header}</td>
             <td>{todonotes.body}</td>
             <td>{todonotes.user_set}</td>
+            <td><button onClick={() => deleteToDoNotes(todonotes.id)} type='button'>DELETE</button></td>
         </tr>
     )
 }
 
-const TodoNotesList = ({todonotes}) => {
+const TodoNotesList = ({todonotes, deleteToDoNotes}) => {
     return(
         <table>
+            <th>ID</th>
             <th>HEADER</th>
             <th>BODY</th>
             <th>USER_SET</th>
-            {todonotes.map((todonotes) => <TodoNotesItem todonotes={todonotes} />)}
+            {todonotes.map((todonotes) => <TodoNotesItem todonotes={todonotes} deleteToDoNotes={deleteToDoNotes}/>)}
         </table>
     )
 }
